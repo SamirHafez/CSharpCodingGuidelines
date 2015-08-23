aad7fe679905436a1c78ff306b1cbd7068a2cec0
SamirHafez <safhafez@gmail.com>
2015-08-24 00:01:57 +0100

    Added AV1522

---
+++ CodingGuidelines/CodingGuidelines/Maintainability/AV1522.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+
+namespace CodingGuidelines.Maintainability
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AV1522 : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "AV1522";
+        internal const string Description = "Assign each variable in a separate statement";
+        internal const string MessageFormat = "Assign each variable in a separate statement";
+        internal const string Category = "Maintainability";
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Description, MessageFormat, Category, DiagnosticSeverity.Warning, true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public ImmutableArray<SyntaxKind> SyntaxKindsOfInterest { get { return ImmutableArray.Create(SyntaxKind.LogicalNotExpression); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.SimpleAssignmentExpression);
+        }
+
+        public void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var assignmentExpression = context.Node as AssignmentExpressionSyntax;
+
+            if (assignmentExpression?.Right is AssignmentExpressionSyntax)
+                context.ReportDiagnostic(Diagnostic.Create(Rule, assignmentExpression.GetLocation()));
+        }
+    }
+} 
