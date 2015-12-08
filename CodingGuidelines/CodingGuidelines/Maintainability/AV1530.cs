23920cb63f56d48bbb86f14d32cfad16c5daef29
SamirHafez <safhafez@gmail.com>
2015-12-08 12:38:15 +0000

    Finished AV1530 + Tests

--- CodingGuidelines/CodingGuidelines/Maintainability/AV1530.cs
+++ CodingGuidelines/CodingGuidelines/Maintainability/AV1530.cs
@@ -2,6 +2,8 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Linq;
 
 namespace DiagnosticAnalyzerAndCodeFix.Maintainability
 {
@@ -19,12 +21,40 @@ namespace DiagnosticAnalyzerAndCodeFix.Maintainability
 
         public override void Initialize(AnalysisContext context)
         {
-            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.ForStatement | SyntaxKind.ForEachStatement);
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.ForEachStatement);
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.ForStatement);
         }
 
         public void AnalyzeNode(SyntaxNodeAnalysisContext context)
         {
+            var node = context.Node;
 
+            if (node is ForEachStatementSyntax)
+            {
+                var forEach = (ForEachStatementSyntax)node;
+                var identifier = forEach.Identifier;
+
+                if (identifier != null)
+                    FindViolatingAssignments(context, forEach, identifier);
+            }
+            else if (node is ForStatementSyntax)
+            {
+                var @for = (ForStatementSyntax)node;
+                var declarationVariables = @for.Declaration?.Variables;
+
+                if (declarationVariables.HasValue)
+                    foreach (var declarationVariable in declarationVariables)
+                        FindViolatingAssignments(context, @for, declarationVariable.Identifier);
+            }
+        }
+
+        private static void FindViolatingAssignments(SyntaxNodeAnalysisContext context, SyntaxNode syntaxNode, SyntaxToken identifier)
+        {
+            var assignments = syntaxNode.DescendantNodes().OfType<AssignmentExpressionSyntax>();
+
+            foreach (var violatingAssignment in assignments.Where(ass => ass.Left is IdentifierNameSyntax
+                                             && ((IdentifierNameSyntax)ass.Left).Identifier.Value == identifier.Value))
+                context.ReportDiagnostic(Diagnostic.Create(Rule, violatingAssignment.GetLocation()));
         }
     }
-} 
+}
