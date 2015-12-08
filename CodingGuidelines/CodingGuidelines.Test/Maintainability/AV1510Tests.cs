af7bd2878218ae5bcff409c54d9c1629e5974356
SamirHafez <safhafez@gmail.com>
2015-12-08 19:55:26 +0000

    Added AV1510 + Tests

---
+++ CodingGuidelines/CodingGuidelines.Test/Maintainability/AV1510Tests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+using Microsoft.CodeAnalysis.Diagnostics;
+using DiagnosticAnalyzerAndCodeFix.Maintainability;
+using System.Linq;
+
+namespace CodingGuidelines.Test.Maintainability
+{
+    [TestClass]
+    public class AV1510Tests : CodeFixVerifier
+    {
+        [TestMethod]
+        public void QualifiedName()
+        {
+            var code = @"
+                        using System;
+                        using System.Linq;
+
+                        namespace ConsoleApp1
+                        {
+                            public class Test0
+                            {
+                                public void Tested()
+                                {
+                                    var list = new System.Collections.Generic.List<string>();
+                                }
+                            }
+                        }
+                        ";
+
+            var expected = new DiagnosticResult
+            {
+                Id = "AV1510",
+                Message = "Use using statements instead of fully qualified type name",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                        new[] {
+                                new DiagnosticResultLocation("Test0.cs", 11, 52)
+                            }
+            };
+
+            // 3 times for: 'System.Collections.Generic.List<>'; 'System.Collections.Generic'; 'System.Collections'
+            VerifyCSharpDiagnostic(code, Enumerable.Range(0, 3).Select(i => expected).ToArray());
+        }
+
+        [TestMethod]
+        public void MemberAccess()
+        {
+            var code = @"
+                        using System;
+                        using System.Linq;
+
+                        namespace ConsoleApp1
+                        {
+                            public class Test0
+                            {
+                                public void Tested()
+                                {
+                                    var memberAccess = String.Empty;
+                                }
+                            }
+                        }
+                        ";
+
+            VerifyCSharpDiagnostic(code);
+        }
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new AV1510();
+        }
+    }
+}
---
+++ CodingGuidelines/CodingGuidelines/Maintainability/AV1510.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace DiagnosticAnalyzerAndCodeFix.Maintainability
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AV1510 : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "AV1510";
+        internal const string Description = "Use using statements instead of fully qualified type name";
+        internal const string MessageFormat = "Use using statements instead of fully qualified type name";
+        internal const string Category = "Maintainability";
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Description, MessageFormat, Category, DiagnosticSeverity.Warning, true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.QualifiedName);
+        }
+
+        public void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            if(context.Node.Parent is UsingDirectiveSyntax)
+                return;
+
+            Diagnostic diagnostic = Diagnostic.Create(Rule, context.Node.GetLocation());
+
+            context.ReportDiagnostic(diagnostic);
+        }
+    }
+}
